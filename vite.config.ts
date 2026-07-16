44020b371c7484e62fdb52d9eaca45e1e987f346
Arbaaz <arbaaz970@gmail.com>
2026-07-16 12:37:17 +0300

    feat: establish project architecture

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest/config" />
 
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
